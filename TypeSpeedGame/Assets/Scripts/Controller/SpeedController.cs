5b03914d3469a31d1f8cf84f66fe1b65165550dd
Başar Ekinci <88886052+BasarEkinci@users.noreply.github.com>
2024-08-04 14:11:34 +0300

    Managers Added

---
+++ TypeSpeedGame/Assets/Scripts/Controller/SpeedController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace Controller
+{
+    public class SpeedController : MonoBehaviour
+    {
+        [SerializeField] private float maxSpeed;
+        [SerializeField] private float acceleration;
+        [SerializeField] private float deceleration;
+        
+        private float _currentSpeed = 0;
+        
+        internal void AddSpeed(float speed)
+        {
+            _currentSpeed += speed;
+            if (_currentSpeed > maxSpeed)
+            {
+                _currentSpeed = maxSpeed;
+            }
+        }
+        
+        internal void SubtractSpeed(float speed)
+        {
+            _currentSpeed -= speed;
+            if (_currentSpeed < 0)
+            {
+                _currentSpeed = 0;
+            }
+        }
+        
+        internal void StopCar()
+        {
+            while (_currentSpeed > 0)
+            {
+                _currentSpeed -= deceleration;
+            }
+        }
+    }
+}
