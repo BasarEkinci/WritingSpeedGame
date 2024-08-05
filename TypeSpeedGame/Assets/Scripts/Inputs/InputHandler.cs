1b8f454664445b7fe9fdbbe2719a5b44636024f0
Başar Ekinci <88886052+BasarEkinci@users.noreply.github.com>
2024-08-06 00:03:54 +0300

    Codes Updated

---
+++ TypeSpeedGame/Assets/Scripts/Inputs/InputHandler.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+namespace Inputs
+{
+    public class InputHandler : MonoBehaviour
+    {
+        private string _playerInput;
+        internal string GetPlayerInput()
+        {
+            _playerInput = "";
+            foreach (char c in Input.inputString) 
+            { 
+                if (c == '\b') 
+                { 
+                    if (_playerInput.Length != 0) 
+                    { 
+                        _playerInput = _playerInput.Substring(0, _playerInput.Length - 1);
+                    }
+                }
+                else 
+                { 
+                    if (Input.inputString != " " && Input.inputString != "\n" && Input.inputString != "\r") 
+                    { 
+                        _playerInput += c;
+                    } 
+                }
+            }
+            return _playerInput;
+        }
+    }
+}
